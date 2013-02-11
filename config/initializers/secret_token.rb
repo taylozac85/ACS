0f8a2c6714cb468c1400378160755c4c24e4dcde
Zach Taylor <taylozac85@gmail.com>
2013-02-11 10:21:55 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirbnbCleaningService::Application.config.secret_token = '2b4441e3ba46f67648deb213623f0d4f52824ee5ddff41e212ad382cb7ef9daa18c87fbf8093abf8b848b6a79ea1e14e46fb013cf3fe8874364377a8670cdfb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AirbnbCleaningService::Application.config.session_store :cookie_store, key: '_Airbnb Cleaning Service_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AirbnbCleaningService::Application.config.session_store :active_record_store
