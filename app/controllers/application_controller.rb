56d4d1f7da58ece27a1991eb452e48c13adc6554
Zach Taylor <taylozac85@gmail.com>
2013-02-19 15:52:02 -0800

    Rollback test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :ensure_domain
-
-  TheDomain = "www.joinhummingbird.com"
-
-  def ensure_domain
-  	if request.env['HTTP_HOST'] != TheDomain
-  		redirect_to TheDomain, :status => :moved_permanently 
-  	end
-  end
 end
