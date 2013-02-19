3f752ea8264b356078daa348d3a25aa6c1ff3456
Zach Taylor <taylozac85@gmail.com>
2013-02-19 14:50:43 -0800

    Add domain redirects to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :ensure_domain
+
+  TheDomain = "www.joinhummingbird.com"
+
+  def ensure_domain
+  	if request.env['HTTP_HOST'] |= TheDomain
+  		redirect_to TheDomain
+  	end
+  ensure_domain
 end
